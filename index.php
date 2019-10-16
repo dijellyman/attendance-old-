18490c980a1afc4c352cd53fc2a6b6f5aa983e6a
Dijellyman <rolest@live.com>
2019-10-16 13:24:57 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,69 @@
+
+<?php
+    $title = 'Index'; 
+
+    require_once 'includes/header.php'; 
+    require_once 'db/conn.php'; 
+
+    $results = $crud->getSpecialties();
+
+?>
+    <!-- 
+        - First name
+        - Last Name
+        - Date of Birth (Use DatePicker)
+        - Specialty (Database Admin, SOftware Developer, Web Administrator, Other)
+        - Email Address
+        - Contact Number
+     -->
+    <h1 class="text-center">2019 International CANNEX Conference </h1>
+
+    <form method="post" action="success.php">
+        <div class="form-group">
+            <label for="firstname">First Name</label>
+            <input required type="text" class="form-control" id="firstname" name="firstname">
+        </div>
+        <div class="form-group">
+            <label for="lastname">Last Name</label>
+            <input required type="text" class="form-control" id="lastname" name="lastname">
+        </div>
+        <div class="form-group">
+            <label for="dob">Date Of Birth</label>
+            <input type="text" class="form-control" id="dob" name="dob">
+        </div>
+        <div class="form-group">
+            <label for="specialty">Area of Expertise</label>
+            <select class="form-control" id="specialty" name="specialty">
+                    <option>Select one</option>
+                    <option>Database Admin</option>
+                    <option>Programmer/Analyst</option>
+                    <option>Website Admin</option>
+                    <option>Graphic Designer</option>
+                    <option>ICT Security</option>
+                    <option>ICT Manager</option>
+                    <option>Ethical Hacker</option>
+                    <option>Social Media Managers</option>
+                <?php while($r = $results->fetch(PDO::FETCH_ASSOC)) {?>
+                   <option value="<?php echo $r['specialty_id'] ?>"><?php echo $r['name']; ?></option>
+                <?php }?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input required type="email" class="form-control" id="email"  name="email" aria-describedby="emailHelp" >
+            <small id="emailHelp" class="form-text text-muted">All information is kept private.</small>
+        </div>
+        <div class="form-group">
+            <label for="phone">Contact Number</label>
+            <input type="text" class="form-control" id="phone" name="phone" aria-describedby="phoneHelp" >
+            <small id="phoneHelp" class="form-text text-muted">All information is kept private.</small>
+        </div>
+        
+        <button type="submit" name="submit" class="btn btn-primary btn-block">Submit</button>
+    </form>
+<br>
+<br>
+<br>
+<br>
+<br>
+<?php require_once 'includes/footer.php'; ?>
